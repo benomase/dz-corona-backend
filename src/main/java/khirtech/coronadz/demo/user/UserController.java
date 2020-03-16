5116fb2db1675281a449031731c6afb14edff485
alitarfa <alitarfa123@gmail.com>
2020-03-16 17:28:03 +0100

    progress login

---
+++ src/main/java/khirtech/coronadz/demo/user/UserController.java
@@ -0,0 +1,8 @@
+package khirtech.coronadz.demo.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
---
+++ src/main/java/khirtech/coronadz/demo/user/UserService.java
@@ -0,0 +1,7 @@
+package khirtech.coronadz.demo.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
